a34e3d9939b15701e22be7e290216c4466fcfd84
Nico L <l.nico08@laposte.net>
2022-03-01 22:08:25 +0900

    new seeds

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,15 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,32 @@ puts 'cleaning up database'
 Flat.destroy_all
 puts 'database is clean'
 
+flats = [
+  { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3 },
+  { name: 'Le Chanel Coco Flat', address: 'Avenue des Champs Elysee', description: 'An awesome flat on the Champs Elysees. Stunning View.', price_per_night: 118, number_of_guests: 2 },
+  { name: 'Lady Gaga', address: '1 rue Bad Romance', description: 'It is very fancy.', price_per_night: 65, number_of_guests: 8 },
+  { name: 'Yaeger House', address: 'Trost District', description: 'A cute little house with something special in the basement. A key is needed', price_per_night: 12, number_of_guests: 4 }
+]
+
+flats.each do |flat|
+  Flat.create! flat
+end
+
+=begin
+
 puts 'creating database'
 4.times do
   flat = Flat.create!(
-    name: ['Light & Spacious Garden Flat London', 'Le Chanel Coco Flat', 'Lady Gaga Villa', 'Eren House'],
-    address: ['10 Clifton Gardens London W9 1DT', '1 rue de Tintin', 'Paradise Island', 'Trost District'],
-    description: ['A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', 'Very average flat, but a nice cute small dog inside', 'Massive villa. The perfect place for partying', 'a family house with something cool in the basement but you need the key'],
-    price_per_night: [75, 85, 32, 99],
-    number_of_guests: [3, 10, 2, 1]
+    name: ['Light & Spacious Garden Flat London', 'Le Chanel Coco Flat', 'Lady Gaga Villa', 'Eren House'].sample,
+    address: ['10 Clifton Gardens London W9 1DT', '1 rue de Tintin', 'Paradise Island', 'Trost District'].sample,
+    description: ['A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', 'Very average flat, but a nice cute small dog inside', 'Massive villa. The perfect place for partying', 'a family house with something cool in the basement but you need the key'].sample,
+    price_per_night: [75, 85, 32, 99].sample,
+    number_of_guests: [3, 10, 2, 1].sample
   )
   puts "flat #{flat.id} is created"
 end
+
+=end
 
 puts 'done'
 
