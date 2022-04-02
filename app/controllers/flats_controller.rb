dc0a200982bf994c374ecfa8f6fb17972a08ef3f
Nico L <l.nico08@laposte.net>
2022-04-02 23:47:10 +0900

    add delete part

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -29,6 +29,11 @@ class FlatsController < ApplicationController
     redirect_to flat_path(@flat)
   end
 
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def set_flat
