ace1bcb0a10a3d13de44b5ae5b38c5fa986354df
Nico L <l.nico08@laposte.net>
2022-04-16 13:28:05 +0900

    adding snk font

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,6 @@ module RailsSimpleAirbnb
 
     # adding customized fonts
     config.assets.enabled = true
-    config.assets.paths << Rails.root.join('/app/assets/fonts')
+    # config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
